de8b5d9961e3a7a98243cf8218abea407b91c5b6
jittrapol <jittrapol.i@psu.ac.th>
2025-09-02 11:01:01 +0700

    Detect the landed state

--- Assets/Scripts/RayCastCube.cs
+++ Assets/Scripts/RayCastCube.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RayCastCube : MonoBehaviour
@@ -7,9 +8,12 @@ public class RayCastCube : MonoBehaviour
     private enum States
     {
         falling,
-        landed
+        landed,
+        die,
+        kill,
+        evade,
+        attack
     }
-
     private States state;
 
     private float height = 4f;
@@ -21,11 +25,11 @@ public class RayCastCube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // state = States.falling;
+        state = States.falling;
         isSlowingDown = false;
         targetMask = LayerMask.GetMask("Target");
         rb = GetComponent<Rigidbody>();
-        // Debug.Log("Status: " + state);
+        Debug.Log("Status: " + state);
     }
 
     // Update is called once per frame
@@ -39,13 +43,13 @@ public class RayCastCube : MonoBehaviour
         //Draw a line to show where the ray is projecting to. Again, we need the position where the line starts and the direction it is pointing to.
         //Note that this Debug.DrawRay() will draw a line which can be seen in the Scene view only.
         Debug.DrawRay(transform.position, Vector3.down * height);
-        
+
 
         // If it is still falling fast, then use the Raycast to detect the floor. If the ray hits the floor, then increases the drag value of the falling cube.
         if (!isSlowingDown)
         {
 
-            if(Physics.Raycast(landingRay, out hit, height, targetMask))
+            if (Physics.Raycast(landingRay, out hit, height, targetMask))
             {
                 if (hit.collider.CompareTag("Floor"))
                 {
@@ -57,12 +61,12 @@ public class RayCastCube : MonoBehaviour
     }
 
     // Check if the falling cube physically touches the red cube. If it does, change the state to "landed".
-    // private void OnCollisionEnter(Collision collision)
-    // {
-    //     if(collision.gameObject.CompareTag("RedCube"))
-    //     {
-    //         state = States.landed;
-    //         Debug.Log("Status: " + state);
-    //     }
-    // }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Floor"))
+        {
+            state = States.landed;
+            Debug.Log("Status: " + state);
+        }
+    }
 }
